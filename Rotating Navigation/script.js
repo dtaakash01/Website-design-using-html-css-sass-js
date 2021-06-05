14d4ab3d375da696afa28154e0f9ffa8cede5331
Akash Dixit <akashdixit@localhost.localdomain>
2021-06-05 10:46:42 +0530

    website html css

---
+++ Rotating Navigation/script.js
@@ -0,0 +1,10 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+console.log(open);
+open.addEventListener('click', () =>{ container.classList.add('show-nav');
+console.log("cli");    
+});
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));
